2dde784467d08a1ad15c535c294238a2dbb9e284
CLOVER-6 <89226564+CLOVER-6@users.noreply.github.com>
2024-01-28 01:46:10 +0000

    Broken comparison
    comparison simply didnt work. It would still write to environment var despite a bad API response

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ userPassword=$3
 userUserKey=$(curl -X POST -d "api_dev_key=$1" -d "api_user_name=$2" -d "api_user_password=$3" "https://pastebin.com/api/api_login.php")
 badApiRequest="Bad API request"
 
-if [[ userUserKey != *badApiRequest* ]]
+if ! grep -q "$badApiRequest" <<< "$userUserKey";
     then
         echo "export PASTEBIN_API_DEV_KEY=$1" >> ~/.profile
         echo "export PASTEBIN_API_USER_KEY=$userUserKey" >> ~/.profile
