db30f059c567c5cdf9249a3e24907ac89126dfe5
CLOVER-6 <89226564+CLOVER-6@users.noreply.github.com>
2024-01-28 19:46:02 +0000

    fixing API request error
    quoted out user data with "bad characters" would cause api errors. finally fixed this

--- setup.sh
+++ setup.sh
@@ -3,15 +3,17 @@
 devKey=$1
 userName=$2
 userPassword=$3
-userUserKey=$(curl -X POST -d "api_dev_key=$1" -d "api_user_name=$2" -d "api_user_password=$3" "https://pastebin.com/api/api_login.php")
+userUserKey=$(curl -X POST --data-urlencode "api_dev_key"="$devKey" --data-urlencode "api_user_name"="$userName" --data-urlencode "api_user_password"="$userPassword" "https://pastebin.com/api/api_login.php")
 badApiRequest="Bad API request"
 
 if ! grep -q "$badApiRequest" <<< "$userUserKey";
     then
-        echo "export PASTEBIN_API_DEV_KEY=$1" >> ~/.profile
+        echo "export PASTEBIN_API_DEV_KEY=$devKey" >> ~/.profile
         echo "export PASTEBIN_API_USER_KEY=$userUserKey" >> ~/.profile
 else
-    echo "A request error occurred, this could be to do with your username/password syntax (see README.md notes) or external problems
+    echo "
 
-	$userUserKey"
+A request error occurred, this could be to do with your username/password syntax (see README.md notes) or external problems
+
+$userUserKey"
 fi
