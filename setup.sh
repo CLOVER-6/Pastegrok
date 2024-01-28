16e1e8c416645b21753e48ebfd72f5ae6f9bd5e0
CLOVER-6 <89226564+CLOVER-6@users.noreply.github.com>
2024-01-28 01:10:50 +0000

    Create setup.sh
    much sleeker and secure setup that simply places API keys into environment variables

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+devKey=$1
+userName=$2
+userPassword=$3
+userUserKey=$(curl -X POST -d "api_dev_key=$1" -d "api_user_name=$2" -d "api_user_password=$3" "https://pastebin.com/api/api_login.php")
+badApiRequest="Bad API request"
+
+if [[ userUserKey != *badApiRequest* ]]
+    then
+        echo "export PASTEBIN_API_DEV_KEY=$1" >> ~/.profile
+        echo "export PASTEBIN_API_USER_KEY=$userUserKey" >> ~/.profile
+else
+    echo "A request error occurred, this could be to do with your username/password syntax (see README.md notes) or external problems
+
+	$userUserKey"
+fi
